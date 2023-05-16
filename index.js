5e25e369d20ff1291cc3358a57933a73f9abc94d
Gustavo <guhenriques1398@gmail.com>
2023-05-16 20:44:38 -0300

    Folder structure changes

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 // index.js server as the entry point of the application. Responsible for initializing. Keep it minimal and primarily responsible for starting the server.
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const { Pool } = require('pg');
 const port = 3000
-const db = require('./queries')
+const express = require('express')
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+require('dotenv').config();
+
+// database connection
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({ info: 'This is my E-commerce Rest API!' })
 })
-
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
 );
+
