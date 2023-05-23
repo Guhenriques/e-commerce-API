6e3cbe36ec2b50ab444acaaf235e35f4918ee5d9
Gustavo <guhenriques1398@gmail.com>
2023-05-23 15:46:38 -0300

    Local login for users set up

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const usersRoutes = require('./routes/usersRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const ordersRoutes = require('./routes/ordersRoutes');
 const checkoutRoutes = require('./routes/checkoutRoutes');
+const loginRouter = require('./routes/authRoute');
+
+const passport = require('passport');
+const session = require('express-session');
+
+app.use(session({
+  secret: 'secretrestapi',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
 
 app.use(express.json());
 
@@ -14,6 +27,7 @@ app.use('/products', productsRoutes);
 app.use('/cart', cartRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/checkout', checkoutRoutes);
+app.use('/login', loginRouter);
 
 app.get('/', (request, response) => {
   response.json({ info: 'This is my E-commerce Rest API!' })
